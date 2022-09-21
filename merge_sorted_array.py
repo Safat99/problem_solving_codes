b596027560e5a7d860a2b43a54ed8703dc1bf32d
Safat99 <sarkersafat99@gmail.com>
2022-09-22 05:09:12 +0600

    improved version added

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -1,35 +1,60 @@
 # m and n can be used as two pointers
+### best solution
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        last = m + n - 1
-        if n==0:
-            return nums1
-        elif m==0:
-            for i in range(n):
-                nums1[i] = nums2[i]
-            return nums1
-        
-        for i in range(last, -1, -1):
-            # print(m,n)
-            if m>0 and n>0:
-                if nums1[m-1] <= nums2[n-1]:
-                    nums1[i] = nums2[n-1]
-                    n -=1
-                else:
-                    nums1[i] = nums1[m-1]
-                    m -=1
-                continue
-            print(m,n)
-            
-            if m == 0:
-                nums1[i] = nums2[n-1]
+        last = m + n - 1       
+        while m>0 and n>0:
+            if nums1[m-1] <= nums2[n-1]:
+                nums1[last] = nums2[n-1]
                 n -= 1
-            elif n == 0:
-                nums1[i] = nums1[m-1]
+            else:
+                nums1[last] = nums1[m-1]
                 m -= 1
+            last -= 1
+            
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n -= 1
+            last -= 1
+
+
+
+## prev solve
+# m and n can be used as two pointers
+# class Solution:
+#     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+#         """
+#         Do not return anything, modify nums1 in-place instead.
+#         """
+#         last = m + n - 1
+#         if n==0:
+#             return nums1
+#         elif m==0:
+#             for i in range(n):
+#                 nums1[i] = nums2[i]
+#             return nums1
+        
+#         for i in range(last, -1, -1):
+#             # print(m,n)
+#             if m>0 and n>0:
+#                 if nums1[m-1] <= nums2[n-1]:
+#                     nums1[i] = nums2[n-1]
+#                     n -=1
+#                 else:
+#                     nums1[i] = nums1[m-1]
+#                     m -=1
+#                 continue
+#             print(m,n)
+            
+#             if m == 0:
+#                 nums1[i] = nums2[n-1]
+#                 n -= 1
+#             elif n == 0:
+#                 nums1[i] = nums1[m-1]
+#                 m -= 1
 
 
 
