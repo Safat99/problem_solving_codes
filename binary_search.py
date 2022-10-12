bcbe4a86be4346b85370c36a8b2d263b07fb4cc2
Safat99 <sarkersafat99@gmail.com>
2022-10-13 05:07:53 +0600

    raw code of binary search

---
+++ binary_search.py
@@ -0,0 +1,16 @@
+class solution:
+	def binray_search(self, x):
+		a = [1,2,3,4,8,5,6]
+		a.sort()
+		n = len(a)
+		x = int('desired number')
+		left_index, right_index = 0, n-1
+		while(left_index <= right_index):
+			mid = left_index + right_index //2
+			if a[mid] == x:
+				return mid, True
+			if a[mid] < x:
+				left_index = mid + 1
+			else:
+				right_index = mid - 1
+		return None
