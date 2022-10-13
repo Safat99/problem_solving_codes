ca7d21c012a7370d3829fa8aac69c30dd5e1c629
Safat99 <sarkersafat99@gmail.com>
2022-10-13 14:48:00 +0600

    binary_search corrected

--- binary_search.py
+++ binary_search.py
@@ -1,12 +1,12 @@
-class solution:
-	def binray_search(self, x):
+class Solution:
+	def binary_search(self, x):
 		a = [1,2,3,4,8,5,6]
 		a.sort()
 		n = len(a)
-		x = int('desired number')
+		#x = int('desired number')
 		left_index, right_index = 0, n-1
 		while(left_index <= right_index):
-			mid = left_index + right_index //2
+			mid = (left_index + right_index) //2
 			if a[mid] == x:
 				return mid, True
 			if a[mid] < x:
